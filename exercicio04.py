22df535e0d935895183817b809849916a54063af
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-22 16:02:15 -0300

    feat exercício-lp-04

--- exercicio04.py
+++ exercicio04.py
@@ -1,4 +1,7 @@
-i = 0
-para  x  no  intervalo ( 101 ):
-    i  =  i  +  x  
-imprimir ( "resultado:" , i )
+soma   = 0
+numero = 0
+for numero in range(1,100):
+    soma = soma + numero
+    print("Número:",numero)
+  
+print("A soma dos números de 1 a 100 foi: ", soma)
