7996985aa650752a215bde74a0d3c42be4c0c8be
ViitzyzZ <braiantomasi0018@outlook.com>
2020-09-22 15:41:17 -0300

    feat exercicio-lp-05

---
+++ exercicio05.py
@@ -0,0 +1,25 @@
+#Exercício 5.
+idadeMediaH = 0 #nao funcionol sem declarar 
+idadeMediaM = 0
+mediaGrupo = 0
+H = 0
+F = 0
+
+for x in range(10):
+    while(True):
+        sexo    = (str(input("Digite o sexo da pessoa (masculino ou feminino):")))
+        idade   = (int(input("Digite a idade da pessoa:")))
+        if sexo.lower() == ("masculino"):
+            idadeMediaH += idade
+            mediaGrupo  += idade
+            H += 1
+            break
+        elif sexo.lower() == ("feminino"):
+            idadeMediaM += idade
+            mediaGrupo  += idade
+            F += 1
+            break
+
+if idadeMediaH>0: print("media de idade dos homens:",idadeMediaH / H)
+if idadeMediaM>0:print("media de idade das mulheres",idadeMediaM / F)
+if mediaGrupo>0:print("media de idade desse grupo:",mediaGrupo / (H + F))
